b103442fc36acaef547ac9656b96171bffd80b47
ItsNaklov <outsider90@gmail.com>
2026-04-11 22:59:34 +0200

    Hello World test refactor

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Murat")
-	want := "Merhaba, Murat"
+t.Run("saying hello to people", func(t *testing.T){
+  got := Hello("Murat")
+	want := "Hello, Murat"
+	assertCorrectMessage(t, got, want)
+})
 
+t.Run("say 'Hello, World' when an emtpy string is supplied", func(t *testing.T){
+	got := Hello("")
+	want := "Hello, World"
+	assertCorrectMessage(t, got, want)
+})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
