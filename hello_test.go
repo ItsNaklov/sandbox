57c540b6d311f4fb92068d244b926cf98c3b255e
ItsNaklov <outsider90@gmail.com>
2026-04-14 22:44:52 +0200

    Hello World phase done

--- hello_test.go
+++ hello_test.go
@@ -3,17 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-t.Run("saying hello to people", func(t *testing.T){
-  got := Hello("Murat")
-	want := "Hello, Murat"
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Murat", "")
+		want := "Hello, Murat"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T){
+	got := Hello("Haydar", "French")
+	want := "Bonjour, Haydar"
 	assertCorrectMessage(t, got, want)
 })
 
-t.Run("say 'Hello, World' when an emtpy string is supplied", func(t *testing.T){
-	got := Hello("")
-	want := "Hello, World"
-	assertCorrectMessage(t, got, want)
-})
+	t.Run("say 'Hello, World' when an emtpy string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
@@ -21,4 +32,3 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-
