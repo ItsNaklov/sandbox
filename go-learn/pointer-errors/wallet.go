e0ca0d025ce916f60b5b695da0a0207d36cbb47e
ItsNaklov <outsider90@gmail.com>
2026-04-21 23:08:44 +0200

    Writing Bitcoin Wallet. Withdraw and Error in the func

--- go-learn/pointer-errors/wallet.go
+++ go-learn/pointer-errors/wallet.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+type Bitcoin int
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
-func (w *Wallet) Balance() int {
-	return (*w).balance
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
 
 }
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
