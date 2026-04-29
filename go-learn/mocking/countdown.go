89f688e91667eb4bcdadd3f7e9ea34c328f1e44f
ItsNaklov <outsider90@gmail.com>
2026-04-30 00:31:15 +0200

    mocking done

--- go-learn/mocking/countdown.go
+++ go-learn/mocking/countdown.go
@@ -18,12 +18,25 @@ type SpySleeper struct {
 	Calls int
 }
 
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 type DefaultSleeper struct{}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
-
 }
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
@@ -31,15 +44,32 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	}
 
 	fmt.Fprint(out, finalWord)
-
-}
-
-func main() {
-	sleeper := &DefaultSleeper{}
-	Countdown(os.Stdout, sleeper)
 }
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
