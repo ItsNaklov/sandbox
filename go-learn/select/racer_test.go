e2b6a739b1df059e6a78ba31c7780390055d400d
ItsNaklov <outsider90@gmail.com>
2026-05-07 23:11:25 +0200

    refactor defer added make it more readable and simpler

--- go-learn/select/racer_test.go
+++ go-learn/select/racer_test.go
@@ -8,19 +8,14 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	
-	slowServer := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-		
-	}))
-	
-	fastServer := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)	
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -28,7 +23,10 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
-
-	slowServer.Close()
-	fastServer.Close()
+}
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
