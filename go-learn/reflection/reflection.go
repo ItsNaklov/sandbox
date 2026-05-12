0a4709f5e34d347d28eaa014d82f5a1cabc19652
ItsNaklov <outsider90@gmail.com>
2026-05-12 23:15:50 +0200

    val, interface{}, Refactor the code

--- go-learn/reflection/reflection.go
+++ go-learn/reflection/reflection.go
@@ -1,5 +1,14 @@
 package main
 
-func walk(x interface{}, fn func(input string)) {
+import "reflect"
 
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
 }
