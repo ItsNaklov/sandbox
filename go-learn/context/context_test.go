f11a9e73ca474823d5da7ed2234eb97593ed2491
ItsNaklov <outsider90@gmail.com>
2026-05-24 00:31:19 +0200

    code pass next step is cleaning

--- go-learn/context/context_test.go
+++ go-learn/context/context_test.go
@@ -8,20 +8,6 @@ import (
 	"time"
 )
 
-type SpyStore struct {
-	response  string
-	cancelled bool
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
 func TestServer(t *testing.T) {
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
 		data := "hello, world"
@@ -42,16 +28,5 @@ func TestServer(t *testing.T) {
 			t.Error("store was not told to cancel")
 		}
 	})
-	//SpyStore still geting error this need to be fixed somehow tutorial seems like not up to date.
-	data := "hello, world"
-	svr := Server(&SpyStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	svr.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
 }
