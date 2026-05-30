a6b92ae6084001a6831f94ad0fb3ec64ca687478
ItsNaklov <outsider90@gmail.com>
2026-05-30 23:33:51 +0200

    maths chapter started.

--- go-learn/go.mod
+++ go-learn/go.mod
@@ -1,3 +1,5 @@
 module sandboxtest.com/test
 
 go 1.25.1
+
+require github.com/quii/learn-go-with-tests v0.0.0-20260509070331-dc00c650f666 // indirect
---
+++ go-learn/maths/clockface.go
@@ -0,0 +1,30 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return float64(t.Second()) * (math.Pi / 30)
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
