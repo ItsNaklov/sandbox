79142614bb65bfdb72d5dd0c8e71437084d0e784
ItsNaklov <outsider90@gmail.com>
2026-05-31 10:34:55 +0200

    fix new file code pass.

--- go-learn/maths/clockface.go
+++ go-learn/maths/clockface.go
@@ -18,7 +18,7 @@ func SecondHand(t time.Time) Point {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return float64(t.Second()) * (math.Pi / 30)
+	return (math.Pi / (30 / float64(t.Second())))
 }
 
 func simpleTime(hours, minutes, seconds int) time.Time {
