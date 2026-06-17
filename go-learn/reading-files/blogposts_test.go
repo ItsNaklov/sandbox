016141f2e41b2f422c3dcfb24b07c6e1bcda593c
ItsNaklov <outsider90@gmail.com>
2026-06-17 23:12:44 +0200

    on the right track! getting the correct error

--- go-learn/reading-files/blogposts_test.go
+++ go-learn/reading-files/blogposts_test.go
@@ -4,6 +4,8 @@ import (
 	"reflect"
 	"testing"
 	"testing/fstest"
+
+	blogposts "sandboxtest.com/test/reading-files"
 )
 
 func TestNewBlogPosts(t *testing.T) {
@@ -11,7 +13,10 @@ func TestNewBlogPosts(t *testing.T) {
 		"hello world.md":  {Data: []byte("Title: Post 1")},
 		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
-
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
 	// rest of test code cut for brevity
 	got := posts[0]
 	want := blogposts.Post{Title: "Post 1"}
