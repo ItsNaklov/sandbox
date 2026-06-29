380bfc9f1e809db97c6f6c86d037ad90a0d713d5
ItsNaklov <outsider90@gmail.com>
2026-06-29 23:53:12 +0200

    assert on strings you get an error

--- go-learn/generics/generics_test.go
+++ go-learn/generics/generics_test.go
@@ -6,6 +6,11 @@ func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
+
+		t.Run("asserting on strings", func(t *testing.T) {
+			AssertEqual(t, "hello", "hello")
+			AssertNotEqual(t, "hello", "Grace")
+		})
 	})
 }
 
