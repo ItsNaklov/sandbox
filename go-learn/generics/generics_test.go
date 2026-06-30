9f7e76f4e090b12a653e2268dac99f105a7dd0f9
ItsNaklov <outsider90@gmail.com>
2026-06-30 14:52:01 +0200

    generics chapter over. generics are simpler than using interface{} in most cases

--- go-learn/generics/generics_test.go
+++ go-learn/generics/generics_test.go
@@ -13,57 +13,12 @@ func TestAssertFunctions(t *testing.T) {
 		AssertNotEqual(t, "hello", "Grace")
 	})
 
-	// AssertEqual(t, 1, "1") // uncomment to see the error
-	t.Run("interface stack dx is horrid", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
-
-		myStackOfInts.Push(1)
-		myStackOfInts.Push(2)
-		firstNum, _ := myStackOfInts.Pop()
-		secondNum, _ := myStackOfInts.Pop()
-
-		// get our ints from out interface{}
-		reallyFirstNum, ok := firstNum.(int)
-		AssertTrue(t, ok) // need to check we definitely got an int out of the interface{}
-
-		reallySecondNum, ok := secondNum.(int)
-		AssertTrue(t, ok) // and again!
-
-		AssertEqual(t, reallyFirstNum+reallySecondNum, 3)
-	})
-}
-
-func AssertEqual[T comparable](t *testing.T, got, want T) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
-func AssertNotEqual[T comparable](t *testing.T, got, want T) {
-	t.Helper()
-	if got == want {
-		t.Errorf("didn't want %v", got)
-	}
-}
-
-func AssertTrue(t *testing.T, got bool) {
-	t.Helper()
-	if !got {
-		t.Errorf("got %v, want true", got)
-	}
-}
-
-func AssertFalse(t *testing.T, got bool) {
-	t.Helper()
-	if got {
-		t.Errorf("got %v, want false", got)
-	}
+	// AssertEqual(t, 1, "1") // uncomment to see the compilation error
 }
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := NewStack[int]()
 
 		// check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
@@ -79,24 +34,12 @@ func TestStack(t *testing.T) {
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
-	})
 
-	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
-
-		// check stack is empty
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-
-		// add a thing, then check it's not empty
-		myStackOfStrings.Push("123")
-		AssertFalse(t, myStackOfStrings.IsEmpty())
-
-		// add another thing, pop it back again
-		myStackOfStrings.Push("456")
-		value, _ := myStackOfStrings.Pop()
-		AssertEqual(t, value, "456")
-		value, _ = myStackOfStrings.Pop()
-		AssertEqual(t, value, "123")
-		AssertTrue(t, myStackOfStrings.IsEmpty())
+		// can get the numbers we put in as numbers, not untyped interface{}
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 }
