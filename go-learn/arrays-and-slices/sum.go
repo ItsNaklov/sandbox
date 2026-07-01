4199691c3c86ce85cff2558d78eed28d1c2f64c0
ItsNaklov <outsider90@gmail.com>
2026-07-01 14:19:26 +0200

    Revisiting arrays and slices with generics

--- go-learn/arrays-and-slices/sum.go
+++ go-learn/arrays-and-slices/sum.go
@@ -20,9 +20,7 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
 		if len(numbers) == 0 {
-
 			sums = append(sums, 0)
-
 		} else {
 			tail := numbers[1:]
 			sums = append(sums, Sum(tail))
@@ -30,4 +28,12 @@ func SumAllTails(numbersToSum ...[]int) []int {
 		}
 	}
 	return sums
+}
+
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	result := initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+	return result
 }
