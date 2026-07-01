7a5aa9534b42bddfce3556fb4da18fd1da34d695
ItsNaklov <outsider90@gmail.com>
2026-07-01 14:39:55 +0200

    assert mv to array file with generics

--- go-learn/arrays-and-slices/sum_test.go
+++ go-learn/arrays-and-slices/sum_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestSum(t *testing.T) {
-
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
@@ -26,26 +25,21 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
-
 		}
 	})
 }
 
 func TestSumAll(t *testing.T) {
-
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
 	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
-
 	}
 }
 
 func TestSumAllTails(t *testing.T) {
-
 	checkSums := func(t testing.TB, got, want []int) {
-
 		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
@@ -56,11 +50,28 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 		checkSums(t, got, want)
-
 	})
 	t.Run("safely sum emtpy slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 		checkSums(t, got, want)
+	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
