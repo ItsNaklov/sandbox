16699ce66f868cf740de409c77cc2addd9389ad9
ItsNaklov <outsider90@gmail.com>
2026-07-02 14:54:24 +0200

    last chapter of tutorial is over.

--- go-learn/arrays-and-slices/sum.go
+++ go-learn/arrays-and-slices/sum.go
@@ -27,11 +27,3 @@ func SumAllTails(numbers ...[]int) []int {
 
 	return Reduce(numbers, sumTail, []int{})
 }
-
-func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
-	result := initialValue
-	for _, x := range collection {
-		result = f(result, x)
-	}
-	return result
-}
--- go-learn/arrays-and-slices/sum_test.go
+++ go-learn/arrays-and-slices/sum_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"reflect"
 	"slices"
+	"strings"
 	"testing"
 )
 
@@ -73,5 +74,35 @@ func TestReduce(t *testing.T) {
 		}
 
 		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+	type Person struct {
+		Name string
+	}
+
+	t.Run("Find the best programmer", func(t *testing.T) {
+		people := []Person{
+			{Name: "Kent Beck"},
+			{Name: "Martin Fowler"},
+			{Name: "Chris James"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Chris")
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Chris James"})
 	})
 }
