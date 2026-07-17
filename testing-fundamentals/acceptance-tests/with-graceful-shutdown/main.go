2c1fa5916509c71d9e74e5f4ddd1fef7248f032b
ItsNaklov <outsider90@gmail.com>
2026-07-17 23:17:14 +0200

    test fundamentals chapter

---
+++ testing-fundamentals/acceptance-tests/with-graceful-shutdown/main.go
@@ -0,0 +1,17 @@
+package main
+
+
+func () {
+	var (
+		ctx = context.Background()
+		httpServer = &http.Server{Addr: ":8080", Handler: http.HandlerFunc(acceptancetests.SlowHandler)}
+		server = gracefulshutdown.NewServer(httpServer)
+	)
+	if err := server.ListenAndServe(ctx); err != nil {
+		// this will typically happen if our responses aren't written before the ctx deadline, not much can be done
+		log.Fatalf("uh oh, didn't shutdown gracefully, some responses may have been lost %v", err)
+
+	}
+	//hopefully, you'll always see this instead
+	log.Println("shutdown gracefully! all responses were sent")
+}
