103b535379e17e709d732d414a7e9dddba257ef4
ItsNaklov <outsider90@gmail.com>
2026-08-07 15:31:13 +0200

    just for understanding.

--- testing-fundamentals/specifications/greeter_server_test.go
+++ testing-fundamentals/specifications/greeter_server_test.go
@@ -4,8 +4,11 @@ import (
 	"testing"
 
 	"testing-fundamentals/specifications"
+
+	go_specs_greet "github.com/quii/go-specs-greet"
 )
 
 func TestGreeterServer(t *testing.T) {
-	specifications.GreetSpecification(t, nil)
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
+	specifications.GreetSpecification(t, driver)
 }
